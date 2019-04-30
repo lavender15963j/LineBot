294107b78b75489ef8db6095b6e9ce40dcb05f1e
Lavender <lavender15963j@gmail.com>
2019-04-30 22:55:58 +0900

    commit

--- main/views.py
+++ main/views.py
@@ -22,7 +22,7 @@ def printDebug(text):
 
 @csrf_exempt
 def elapp(request):
-    printDebug(str(request.POST))
+    printDebug(request.body.decode('utf-8'))
     return HttpResponse("GET")
     
     (mid,text)=_decode_json(request)
